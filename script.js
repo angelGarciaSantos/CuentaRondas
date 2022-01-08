6fce3df168ab0610ff6878e12088464cf5982fae
Ángel García Santos <angelgarciasantos@MacBook-Air-de-Angel.local>
2022-01-08 13:01:06 +0100

    style and interface changes

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ $('#nuevo').on('click', function() {
 	$('tbody').append(`
         <tr>
             <td>`+ $('input').val() +`</td>
+            <td><button class="menos">-</button></td>
             <td class="cantidad">0</td> 
             <td><button class="mas">+</button></td> 
-            <td><button class="menos">-</button></td> 
             <td><button class="borrar">Borrar</button></td> 
         </tr>
     `);
@@ -14,7 +14,7 @@ $(document).on('click', '.mas', function() {
     $(this).parent().prev().html(parseInt($(this).parent().prev().text()) + 1);
 });
 $(document).on('click', '.menos',  function() {
-    $(this).parent().prev().prev().html(parseInt($(this).parent().prev().prev().text()) - 1);
+    $(this).parent().next().html(parseInt($(this).parent().next().text()) - 1);
 });
 $(document).on('click', '.borrar', function() {
     $(this).closest("tr")
