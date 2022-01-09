14073f34cc275c0e7bf1c7529ed0ad79c349b462
Ángel García Santos <angelgarciasantos@MacBook-Air-de-Angel.local>
2022-01-09 18:08:10 +0100

    cookie testing

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ $('#nuevo').on('click', function() {
     `);
     $('input').val('');
     var x = $('table').prop('outerHTML');     
-    $.cookie('tabledata', x);
+    $.cookie('tabledata', x, { expires: 7 });
 });
 $(document).ready(function(){
     let content = $.cookie('tabledata');
@@ -20,22 +20,22 @@ $(document).ready(function(){
 $(document).on('click', '.mas', function() {
     $(this).parent().prev().html(parseInt($(this).parent().prev().text()) + 1);
     var x = $('table').prop('outerHTML');     
-    $.cookie('tabledata', x);
+    $.cookie('tabledata', x, { expires: 7 });
 });
 $(document).on('click', '.menos',  function() {
     $(this).parent().next().html(parseInt($(this).parent().next().text()) - 1);
     var x = $('table').prop('outerHTML');     
-    $.cookie('tabledata', x);
+    $.cookie('tabledata', x, { expires: 7 });
 });
 $(document).on('click', '.borrar', function() {
     $(this).closest("tr")
            .remove();
     var x = $('table').prop('outerHTML');     
-    $.cookie('tabledata', x);
+    $.cookie('tabledata', x, { expires: 7 });
 });
 $(document).on('click', '.guardar', function() {
     var x = $('table').prop('outerHTML');     
-    $.cookie('tabledata', x);
+    $.cookie('tabledata', x, { expires: 7 });
 });
 $(document).on('click', '.cargar', function() {
     let content = $.cookie('tabledata');
