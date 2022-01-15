36f42483554e7396242f4c1739abd0cca07a5c85
Ángel García Santos <angelgarciasantos@MacBook-Air-de-Angel.local>
2022-01-15 17:48:56 +0100

    code cleaning

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Save new drinker
 $('#nuevo').on('click', function() {
 	$('tbody').append(`
         <tr>
@@ -12,36 +13,50 @@ $('#nuevo').on('click', function() {
     var x = $('table').prop('outerHTML');     
     $.cookie('tabledata', x, { expires: 7 });
 });
+
+// Retrieve data from cookies into table after page load
 $(document).ready(function(){
     let content = $.cookie('tabledata');
     if (content!=='undefined')
         $('table').html(content);
 });
+
+// Increase amount
 $(document).on('click', '.mas', function() {
     $(this).parent().prev().html(parseInt($(this).parent().prev().text()) + 1);
     var x = $('table').prop('outerHTML');     
     $.cookie('tabledata', x, { expires: 7 });
 });
+
+// Decrease amount
 $(document).on('click', '.menos',  function() {
     $(this).parent().next().html(parseInt($(this).parent().next().text()) - 1);
     var x = $('table').prop('outerHTML');     
     $.cookie('tabledata', x, { expires: 7 });
 });
+
+// Delete drinker
 $(document).on('click', '.borrar', function() {
     $(this).closest("tr")
            .remove();
     var x = $('table').prop('outerHTML');     
     $.cookie('tabledata', x, { expires: 7 });
 });
+
+// Save table on cookie manually
 $(document).on('click', '.guardar', function() {
     var x = $('table').prop('outerHTML');     
     $.cookie('tabledata', x, { expires: 7 });
 });
+
+// Load cookie on table manually
 $(document).on('click', '.cargar', function() {
     let content = $.cookie('tabledata');
     $('table').html(content);
     alert(content);
 });
+
+
 // $(document).on('click', '.csv', function() {
 //     $("table").table2csv({
 //         filename:'table.csv'
